932d176106f36944c3f60d5ebe90afb459337981
wenlitao <317545417@qq.com>
2021-01-12 15:45:49 +0800

    增加新的代码

---
+++ Hello.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class Hello
+{
+    public function __construct()
+    {
+
+    }
+
+}
---
+++ Test.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class Test
+{
+    public function __construct()
+    {
+        echo time();
+    }
+
+}
